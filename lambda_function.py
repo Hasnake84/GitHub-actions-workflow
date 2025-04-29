13a1d476f12923003792d21777ee4adef4c542c2
Henoks100 <sahlu.henok@hotmail.com>
2025-04-29 17:44:15 -0500

    NNNNOW

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Whats going on man')
+        'body': json.dumps('Whats going on?')
     }# test trigger
 # final trigger test
